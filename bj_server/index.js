1cb15f85f44178272a42c4e74b912cf02713dfa4
mkretkowski131 <318032@stud.umk.pl>
2024-02-20 16:51:29 +0100

    working lobby, some functions in diffrent place

--- bj_server/index.js
+++ bj_server/index.js
@@ -6,7 +6,6 @@ const { join } = require('node:path');
 
 const { Server } = require('socket.io');
 
-
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
@@ -18,23 +17,38 @@ const io = new Server(server, {
 io.on('connection', (socket) => {
   console.log('a user connected');
 
-
-  //WE SHOULD CHECK IF THE ROOM EXIST OR NOT 
-  //IF NOT WE SHOULD GIVE INFO BACK AND SEND ERROR RESPONSE
-  //NOW CLIENT WHO IS TRYING TO JOIN THE ROOM WILL CREATE THE ROOM
-  socket.on("connect_to_room", room_id =>{
+  socket.on("connect_to_room", room_id => {
     if(room_id == ""){
       room_id = "room" + Math.floor(Math.random() * 1000);
     }
   
-    socket.join("ROOM ID: ", room_id)
-    console.log(room_id)
+    socket.join(room_id)
+    console.log("ROOM ID: ", room_id)
   
-    socket.emit("get_room_id", room_id)
+    io.to(room_id).emit("get_room_id", room_id)
   })
-  
 
+  socket.on("get_player_list", (room_id) => {
+
+    console.log("WE GOT:", room_id);
+
+    const room = io.sockets.adapter.rooms.get(room_id);
+    if (room) {
+      const playerList = Array.from(room);
+
+      io.to(room_id).emit("send_player_list", playerList);
+      console.log(room);
+    } else {
+      console.log("Room not found or empty");
+    }
+  })
+
+  socket.on("disconnect", (reason) => {
+    console.log("PLAYER DISCONNECTED")
+  });
 });
+
+
 
 
 app.get('/', (req, res) => {
