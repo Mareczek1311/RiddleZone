31130b25d3f1036f75c5b7d07ff7faf6004abde2
MateuszKretkowski <mkretkowski13@gmail.com>
2024-03-03 18:23:20 +0100

    Question Page, Question Set Page, Responsiveness (Whole Website)

--- bj_web_client/src/app/components/QuestionPage/QuestionPage.tsx
+++ bj_web_client/src/app/components/QuestionPage/QuestionPage.tsx
@@ -1,40 +1,73 @@
 import "./QuestionPage.css";
+import { motion } from "framer-motion";
 
 interface QuestionPageProps {
-    socket: any;
-    room_id: string;
-    currQuestion: string[];
+  socket: any;
+  room_id: string;
+  currQuestion: string[];
 }
 
+const QuestionPage: React.FC<QuestionPageProps> = ({
+  socket,
+  room_id,
+  currQuestion,
+}) => {
+  function sendAnswer(answer: string) {
+    socket.emit("send_answer", { room_id, answer });
+  }
 
-const QuestionPage: React.FC<QuestionPageProps> = ({socket, room_id, currQuestion}) => {
- 
-    function sendAnswer(answer: string) {
-        socket.emit('send_answer', {room_id, answer});
-        
-    }
+  return (
+    <motion.div className="MainSectionLobby">
+      <motion.div className="ManageSectionLobby">
 
-    return(
-        <div>
-            <h1>{currQuestion[0]}</h1>
-
-            <div className="question-page-container">
-                <div className="question-page-answer">
-                    <button onClick={() => {sendAnswer("a")}} className="question-page-answer-button">{currQuestion[1]}</button>
-                </div>
-                <div className="question-page-answer">
-                    <button onClick={() => {sendAnswer("b")}} className="question-page-answer-button">{currQuestion[2]}</button>
-                </div>
-                <div className="question-page-answer">
-                    <button onClick={() => {sendAnswer("c")}} className="question-page-answer-button">{currQuestion[3]}</button>
-                </div>
-                <div className="question-page-answer">
-                    <button onClick={() => {sendAnswer("d")}} className="question-page-answer-button">{currQuestion[4]}</button>
-                </div>
-            </div>
-
-        </div>
-    )
-}
+        <motion.h1>{currQuestion[0]}</motion.h1>
+        <motion.div className="question-page-container">
+          <motion.div className="question-page-answer">
+            <motion.button
+              onClick={() => {
+                sendAnswer("a");
+              }}
+              className="button"
+            >
+              <motion.h1 className="button-text thinner">
+                {currQuestion[1]}
+              </motion.h1>
+            </motion.button>
+          </motion.div>
+          <motion.div className="question-page-answer">
+            <motion.button
+              onClick={() => {
+                sendAnswer("b");
+              }}
+              className="button"
+            >
+              <motion.h1 className="button-text">{currQuestion[2]}</motion.h1>
+            </motion.button>
+          </motion.div>
+          <motion.div className="question-page-answer">
+            <motion.button
+              onClick={() => {
+                sendAnswer("c");
+              }}
+              className="button"
+            >
+              <motion.h1 className="button-text">{currQuestion[3]}</motion.h1>
+            </motion.button>
+          </motion.div>
+          <motion.div className="question-page-answer">
+            <motion.button
+              onClick={() => {
+                sendAnswer("d");
+              }}
+              className="button"
+            >
+              <motion.h1 className="button-text">{currQuestion[4]}</motion.h1>
+            </motion.button>
+          </motion.div>
+        </motion.div>
+      </motion.div>
+    </motion.div>
+  );
+};
 
 export default QuestionPage;
